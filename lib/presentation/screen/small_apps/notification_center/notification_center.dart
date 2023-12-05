7755efa3ad6f30a33aaf0981d286c3528884822b
Woogear Kwon <techkwon1717@hanmail.net>
2023-12-05 16:40:36 +0900

    [UPDATE] blur 애니메이션 적용

--- lib/presentation/screen/small_apps/notification_center/notification_center.dart
+++ lib/presentation/screen/small_apps/notification_center/notification_center.dart
@@ -1,43 +1,115 @@
-import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-
 import 'package:flutter_note/exports.dart';
 
-class NotificationCenterScreen extends StatelessWidget {
+class NotificationCenterScreen extends StatefulWidget {
   const NotificationCenterScreen({super.key});
+
+  @override
+  State<NotificationCenterScreen> createState() =>
+      _NotificationCenterScreenState();
+}
+
+class _NotificationCenterScreenState extends State<NotificationCenterScreen>
+    with SingleTickerProviderStateMixin {
+  Key? _selectedKey;
+  NotificationCard? _selectedWidget;
+
+  late AnimationController _blurController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _blurController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+    );
+    _animation = Tween<double>(begin: 0, end: 10).animate(_blurController);
+    _animation.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
+  }
+
+  @override
+  void dispose() {
+    _blurController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: _backgroundContainer(
-          child: Container(
-            margin: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                const SizedBox(height: 50),
-                const Text(
-                  'Thursday, September 30',
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.white,
+        child: Stack(
+          children: [
+            _backgroundContainer(
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 50),
+                    const Text(
+                      'Thursday, September 30',
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      '8:23',
+                      style: TextStyle(
+                          fontSize: 100,
+                          height: 1.1,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 70),
+                    _notifications(
+                      onClickCard: (key, widget) {
+                        setState(() {
+                          if (_selectedKey == key) {
+                            _selectedKey = null;
+                            _selectedWidget = null;
+                            _blurController.reverse();
+                          } else {
+                            _selectedKey = key;
+                            _selectedWidget = widget;
+                            _blurController.forward();
+                          }
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+            if (_selectedWidget != null)
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  padding: const EdgeInsets.all(13),
+                  child: IntrinsicHeight(
+                    child: NotificationCard(
+                      selected: true,
+                      title: _selectedWidget!.title,
+                      subTitle: _selectedWidget!.subTitle,
+                      time: _selectedWidget!.time,
+                      iconRes: _selectedWidget!.iconRes,
+                      onClick: _selectedWidget!.onClick,
+                      key: _selectedWidget!.key,
+                    ),
                   ),
                 ),
-                const Text(
-                  '8:23',
-                  style: TextStyle(
-                      fontSize: 100,
-                      height: 1.1,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 30),
-                _notifications()
-              ],
-            ),
-          ),
+              ),
+          ],
         ),
       ),
     );
@@ -49,13 +121,24 @@ class NotificationCenterScreen extends StatelessWidget {
       fit: StackFit.expand,
       children: [
         const Image(
-            image: AssetImage(Assets.samBackgroundImage), fit: BoxFit.cover),
-        child
+          image: AssetImage(Assets.samBackgroundImage),
+          fit: BoxFit.cover,
+        ),
+        child,
+        BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaY: _animation.value,
+            sigmaX: _animation.value,
+          ),
+          child: Container(),
+        ),
       ],
     );
   }
 
-  Widget _notifications() {
+  Widget _notifications({
+    required OnClickCard onClickCard,
+  }) {
     return Column(
       children: [
         Container(
@@ -65,7 +148,10 @@ class NotificationCenterScreen extends StatelessWidget {
             children: [
               Text(
                 'Notification Center',
-                style: TextStyle(fontSize: 26, color: Colors.white),
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.white,
+                ),
               )
             ],
           ),
@@ -73,12 +159,14 @@ class NotificationCenterScreen extends StatelessWidget {
         const SizedBox(height: 10),
         NotificationCard(
           title: '리디',
-          subTitle: '<세이노의 가르침> 다운로드 성공',
+          subTitle: '<공자의 가르침> 다운로드 성공',
           time: 'now',
           iconRes: IconRes(
             iconData: Icons.menu_book_rounded,
             color: Palette.paletteBlue100,
           ),
+          onClick: (key, widget) => onClickCard(key, widget),
+          key: UniqueKey(),
         ),
         NotificationCard(
           title: '긴급재난문자',
@@ -89,110 +177,141 @@ class NotificationCenterScreen extends StatelessWidget {
             iconData: Icons.warning,
             color: Palette.paletteYellow100,
           ),
+          onClick: (key, widget) => onClickCard(key, widget),
+          key: UniqueKey(),
         ),
         NotificationCard(
-          title: '책한민국',
+          title: '대한민국',
           subTitle: '애프터 라이프(1부) /브루스 그레이슨 / 현대지성',
           time: '12:35 AM',
           iconRes: IconRes(
             iconData: Icons.play_circle_filled,
             color: Palette.red03Basic,
           ),
+          onClick: (key, widget) => onClickCard(key, widget),
+          key: UniqueKey(),
         ),
         NotificationCard(
           title: '리디',
-          subTitle: '<세이노의 가르침> 다운로드 성공',
+          subTitle: '<맹자의 가르침> 다운로드 성공',
           time: 'now',
           iconRes: IconRes(
             iconData: Icons.menu_book_rounded,
             color: Palette.paletteBlue100,
           ),
+          onClick: (key, widget) => onClickCard(key, widget),
+          key: UniqueKey(),
         ),
       ],
     );
   }
 }
 
+typedef OnClickCard = Function(
+  Key? key,
+  NotificationCard widget,
+);
+
+@immutable
 class NotificationCard extends StatelessWidget {
+  final bool selected;
   final String title;
   final String subTitle;
   final String time;
   final IconRes iconRes;
+  final OnClickCard onClick;
 
-  const NotificationCard({
+  NotificationCard({
+    this.selected = false,
     required this.title,
     required this.subTitle,
     required this.time,
     required this.iconRes,
+    required this.onClick,
     super.key,
   });
 
+  final GlobalKey gKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 10),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Palette.white.withAlpha(130),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 22, sigmaY: 22),
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 13),
-            child: Row(
-              children: [
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: iconRes.color,
-                    borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: () {
+        onClick(key, this);
+
+        // RenderBox box = gKey.currentContext?.findRenderObject() as RenderBox;
+        // Offset position = box.localToGlobal(Offset.zero);
+        // double y = position.dy;
+        // print('woogear position = $position');
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: selected ? Palette.white : Palette.white.withAlpha(150),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: selected ? 0 : 20,
+              sigmaY: selected ? 0 : 20,
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(13),
+              child: Row(
+                children: [
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: iconRes.color,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Icon(iconRes.iconData),
                   ),
-                  child: Icon(iconRes.iconData),
-                ),
-                const SizedBox(width: 15),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            title,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              title,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            time,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              fontSize: 13,
-                              color: Palette.paletteGray10050p,
+                            Text(
+                              time,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 13,
+                                color: Palette.paletteGray10050p,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        subTitle,
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14,
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          subTitle,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -200,46 +319,6 @@ class NotificationCard extends StatelessWidget {
     );
   }
 }
-
-final List<Widget> _notificationContents = [
-  NotificationCard(
-    title: '리디',
-    subTitle: '<세이노의 가르침> 다운로드 성공',
-    time: 'now',
-    iconRes: IconRes(
-      iconData: Icons.menu_book_rounded,
-      color: Palette.paletteBlue100,
-    ),
-  ),
-  NotificationCard(
-    title: '긴급재난문자',
-    subTitle:
-        '[기상청] 11월30일04:55 경북 경주시 동남동쪽 19km 지역 규모4.3 지진발생/낙하물 주의, 국민재난안전포털 행동ㅇ요령에 따라 대응, 여진주의',
-    time: '3h ago',
-    iconRes: IconRes(
-      iconData: Icons.warning,
-      color: Palette.paletteYellow100,
-    ),
-  ),
-  NotificationCard(
-    title: '책한민국',
-    subTitle: '애프터 라이프(1부) /브루스 그레이슨 / 현대지성',
-    time: '12:35 AM',
-    iconRes: IconRes(
-      iconData: Icons.play_circle_filled,
-      color: Palette.red03Basic,
-    ),
-  ),
-  NotificationCard(
-    title: '리디',
-    subTitle: '<세이노의 가르침> 다운로드 성공',
-    time: 'now',
-    iconRes: IconRes(
-      iconData: Icons.menu_book_rounded,
-      color: Palette.paletteBlue100,
-    ),
-  ),
-];
 
 class IconRes {
   final IconData iconData;
